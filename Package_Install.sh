ae99d81e829d42ef4c368ca9efafe32440f810d1
Ruihua Cai <crh0126@gmail.com>
2022-08-01 18:28:06 -0700

    Add Package_Install.sh; Remove README_GPS

---
+++ Package_Install.sh
@@ -0,0 +1,3 @@
+sudo apt install gpsd gpsd-clients python-gps
+sudo apt-get install ros-melodic-nmea-navsat-driver   
+sudo apt-get install ros-melodic-robot-localization
--- src/gps_module/README_GPS.md
+++
@@ -1,4 +0,0 @@
-### GPS Node Package
-`` sudo apt install gpsd gpsd-clients python-gps ``
-### GPS ROS Package
-``sudo apt-get install ros-melodic-nmea-navsat-driver   ``
