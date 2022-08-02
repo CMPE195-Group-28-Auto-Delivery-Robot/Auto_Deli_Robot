15abb9e9a50839154e5e4b4c4ab778bde7630825
TonyHuang <ruixiang.huang1999@gmail.com>
2022-08-01 22:33:17 -0700

    Add auto confirmation for apt

--- Package_Install.sh
+++ Package_Install.sh
@@ -1,6 +1,6 @@
-apt install gpsd gpsd-clients python-gps
-apt-get install ros-melodic-nmea-navsat-driver   
-apt-get install ros-melodic-robot-localization
-apt-get install ros-melodic-mapviz
-apt install ros-melodic-mapviz-plugins
-apt install ros-melodic-tile-map
+apt install gpsd gpsd-clients python-gps -y
+apt-get install ros-melodic-nmea-navsat-driver -y   
+apt-get install ros-melodic-robot-localization -y
+apt-get install ros-melodic-mapviz -y
+apt install ros-melodic-mapviz-plugins -y
+apt install ros-melodic-tile-map -y
