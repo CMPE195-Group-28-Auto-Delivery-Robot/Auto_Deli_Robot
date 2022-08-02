ca88e708e3b6fb098ca9caaaa707460261fa8b2a
Ruihua Cai <crh0126@gmail.com>
2022-08-01 19:07:25 -0700

    And Mapviz

--- Package_Install.sh
+++ Package_Install.sh
@@ -1,3 +1,6 @@
-sudo apt install gpsd gpsd-clients python-gps
-sudo apt-get install ros-melodic-nmea-navsat-driver   
-sudo apt-get install ros-melodic-robot-localization
+apt install gpsd gpsd-clients python-gps
+apt-get install ros-melodic-nmea-navsat-driver   
+apt-get install ros-melodic-robot-localization
+apt-get install ros-melodic-mapviz
+apt install ros-melodic-mapviz-plugins
+apt install ros-melodic-tile-map
