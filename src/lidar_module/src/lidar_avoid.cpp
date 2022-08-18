e3b1151f273d60d975569d19ba6655f27f4d58ca
Ruihua Cai <crh0126@gmail.com>
2022-08-17 20:31:35 -0700

    Converter Node Generate

--- src/lidar_module/src/lidar_avoid.cpp
+++ src/lidar_module/src/lidar_avoid.cpp
@@ -7,27 +7,51 @@
 
 #define RAD2DEG(x) ((x)*180./M_PI)
 
-laser_geometry::LaserProjection Projecter;
-
-void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan)
-{
-    int count = scan->scan_time / scan->time_increment;
-    ROS_INFO("I heard a laser scan %s[%d]:", scan->header.frame_id.c_str(), count);
-    ROS_INFO("angle_range, %f, %f", RAD2DEG(scan->angle_min), RAD2DEG(scan->angle_max));
-    
+class scanCovertCloud{
+private:
+    laser_geometry::LaserProjection Projecter;
     sensor_msgs::PointCloud CloudMap;
-    Projecter.projectLaser(*scan, CloudMap);
-    for(auto point: CloudMap.points){
-        ROS_INFO(": [%f, %f]", point.x, point.y);
+
+public:
+    scanCovertCloud(){
+
     }
-}
+
+    void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan)
+    {
+        int count = scan->scan_time / scan->time_increment;
+        ROS_INFO("I heard a laser scan %s[%d]:", scan->header.frame_id.c_str(), count);
+        ROS_INFO("angle_range, %f, %f", RAD2DEG(scan->angle_min), RAD2DEG(scan->angle_max));
+
+        Projecter.projectLaser(*scan, CloudMap);
+        for(auto point: CloudMap.points){
+            ROS_INFO(": [%f, %f, %f]", point.x, point.y, point.z);
+        }
+    }
+
+    sensor_msgs::PointCloud getCloudMsgs(){
+        return CloudMap;
+    }
+};
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "rplidar_node_client");
+    ros::init(argc, argv, "LaserScan_to_PointCloud");
     ros::NodeHandle n;
 
-    ros::Subscriber sub = n.subscribe<sensor_msgs::LaserScan>("/scan", 1000, scanCallback);
+    ros::Publisher CloudMap_pub = n.advertise<sensor_msgs::PointCloud>("CloudMap", 1000);
+
+    scanCovertCloud Converter;
+
+    ros::Subscriber sub = n.subscribe<sensor_msgs::LaserScan>("/scan", 1000, &scanCovertCloud::scanCallback, &Converter);
+
+    ros::Rate loop_rate(10);
+
+    while(ros::ok()){
+        CloudMap_pub.publish(Converter.getCloudMsgs());
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
 
     ros::spin();
 
