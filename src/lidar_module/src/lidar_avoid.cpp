f7d8d376e73fa8667261aa813c297bc89d13b31b
Ruihua Cai <crh0126@gmail.com>
2022-08-17 20:08:28 -0700

    Convert to xy cordinate

--- src/lidar_module/src/lidar_avoid.cpp
+++ src/lidar_module/src/lidar_avoid.cpp
@@ -1,17 +1,24 @@
+#include <cmath>
 #include "ros/ros.h"
 #include "sensor_msgs/LaserScan.h"
+#include "sensor_msgs/PointCloud.h"
+#include "laser_geometry/laser_geometry.h"
+
 
 #define RAD2DEG(x) ((x)*180./M_PI)
+
+laser_geometry::LaserProjection Projecter;
 
 void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan)
 {
     int count = scan->scan_time / scan->time_increment;
     ROS_INFO("I heard a laser scan %s[%d]:", scan->header.frame_id.c_str(), count);
     ROS_INFO("angle_range, %f, %f", RAD2DEG(scan->angle_min), RAD2DEG(scan->angle_max));
-  
-    for(int i = 0; i < count; i++) {
-        float degree = RAD2DEG(scan->angle_min + scan->angle_increment * i);
-        ROS_INFO(": [%f, %f]", degree, scan->ranges[i]);
+    
+    sensor_msgs::PointCloud CloudMap;
+    Projecter.projectLaser(*scan, CloudMap);
+    for(auto point: CloudMap.points){
+        ROS_INFO(": [%f, %f]", point.x, point.y);
     }
 }
 
@@ -25,4 +32,4 @@ int main(int argc, char **argv)
     ros::spin();
 
     return 0;
-}
+} 
