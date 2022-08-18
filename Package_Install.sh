635bbd8aaabfc69afb92f56fea6819ab71e1d387
Ruihua Cai <crh0126@gmail.com>
2022-08-17 23:26:09 -0700

    Add Basic GUI Implementation

--- Package_Install.sh
+++ Package_Install.sh
@@ -5,6 +5,7 @@ apt-get install ros-melodic-mapviz -y
 apt install ros-melodic-mapviz-plugins -y
 apt install ros-melodic-tile-map -y
 apt-get install ros-melodic-rplidar-ros -y
+sudo apt install python-tornado python-pip ros-melodic-rosbridge-server ros-melodic-web-video-server nginx -y
 cd src/
 git clone https://github.com/stereolabs/zed-ros-examples.git
 # git clone https://github.com/osrf/rvizweb/
