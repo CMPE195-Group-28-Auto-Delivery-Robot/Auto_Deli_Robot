62116fd0edb19feaed6324021e6d4c6699333939
Ruihua Cai <crh0126@gmail.com>
2022-09-06 23:20:55 -0700

    Add IMU Subcribe

--- src/position_module/src/pid_processor.cpp
+++ src/position_module/src/pid_processor.cpp
@@ -10,6 +10,7 @@ class pidController{
 private:
     geometry_msgs::Twist m_robotProcessMsg;
     geometry_msgs::Twist m_robotControlMsg;
+    sensor_msgs::Imu m_robotImuMsg;
     nav_msgs::Odometry m_robotOdometryMsg;
 
 public:
@@ -22,13 +23,18 @@ public:
         m_robotOdometryMsg = *(odomMsg.get());
     }
 
+    void IMUCallback(const sensor_msgs::Imu::ConstPtr& imuMsg)
+    {
+        m_robotImuMsg = *(imuMsg.get());
+    }
+
     void ControlCallback(const geometry_msgs::Twist::ConstPtr& controlMsg)
     {
         m_robotControlMsg = *(controlMsg.get());
     }
 
     geometry_msgs::Twist GetProcessdMsg(){
-        ROS_INFO("Speed: %.5f, %.5f, %.5f, %.5f", m_robotControlMsg.linear.x, m_robotControlMsg.angular.z, m_robotOdometryMsg.twist.twist.linear.x, m_robotOdometryMsg.twist.twist.angular.z);
+        ROS_INFO("Speed: %.5f, %.5f, %.5f, %.5f", m_robotImuMsg.linear_acceleration.x, m_robotImuMsg.angular_velocity.z, m_robotOdometryMsg.twist.twist.linear.x, m_robotOdometryMsg.twist.twist.angular.z);
         return m_robotProcessMsg;
     }
 };
@@ -41,7 +47,9 @@ int main(int argc, char **argv)
     pidController pidNode;
     
     ros::Publisher cmdVel_pub = rosHandle.advertise<geometry_msgs::Twist>("cmd_vel", 1000);
-    ros::Subscriber odom_sub = rosHandle.subscribe<nav_msgs::Odometry>("/zed_node/odom", 1000, &pidController::OdomCallback, &pidNode);
+    ros::Subscriber odom_sub = rosHandle.subscribe<nav_msgs::Odometry>("zed_node/odom", 1000, &pidController::OdomCallback, &pidNode);
+    ros::Subscriber imu_sub = rosHandle.subscribe<sensor_msgs::Imu>("zed_node/imu/data", 1000, &pidController::IMUCallback, &pidNode);
+    ros::Subscriber control_sub = rosHandle.subscribe<geometry_msgs::Twist>("controlVel", 1000, &pidController::ControlCallback, &pidNode);
 
     ros::Rate loop_rate(15);
 
