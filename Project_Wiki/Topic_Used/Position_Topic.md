c55b049955452f00dd2687c5e3e50b8109723b88
skywalkerRex <crh0126@gmail.com>
2022-10-03 19:57:09 -0700

    34 robot position determine (#35)
    * Add Position Module
    
    * Postion Calculation Entry Point
    
    * Add localization template
    
    * Add CPP node implement
    
    * Add IMU Subcribe
    
    * Update Field Test Launch
    
    * Move Field Test to Project Core
    
    * Add Odometry Node
    
    * Update Robot StartUp launch
    
    * Add PID Controller Entry Point

---
+++ Project_Wiki/Topic_Used/Position_Topic.md
@@ -0,0 +1,14 @@
+### /deli_robot/zed_node/pose_with_covariance   
+    * Freq: 15   
+    * Type: geometry_msgs/PoseWithCovarianceStamped  
+    * Info: Position(x,y,z) orientation(x,y,z,w) 
+
+### /deli_robot/zed_node/imu/data   
+    * Freq: 200  
+    * Type: sensor_msgs/Imu 
+    * Info: LinearAcc(x, y, z) AngAcc(x, y, z) 
+
+### /deli_robot/zed_node/imu/mag   
+    * Freq: 50  
+    * Type: sensor_msgs/MagneticField 
+    * Info: MagField(x, y, z) 
---
+++ src/position_module/src/pid_processor.cpp
@@ -0,0 +1,57 @@
+#include <cmath>
+#include "ros/ros.h"
+#include "sensor_msgs/Imu.h"
+#include "nav_msgs/Odometry.h"
+#include "geometry_msgs/Twist.h"
+
+#define RAD2DEG(x) ((x)*180./M_PI)
+
+class pidController{
+private:
+    geometry_msgs::Twist m_robotProcessMsg;
+    geometry_msgs::Twist m_robotControlMsg;
+    nav_msgs::Odometry m_robotOdometryMsg;
+
+public:
+    pidController(){
+
+    }
+
+    void OdomCallback(const nav_msgs::Odometry::ConstPtr& odomMsg)
+    {
+        m_robotOdometryMsg = *(odomMsg.get());
+    }
+
+    void ControlCallback(const geometry_msgs::Twist::ConstPtr& controlMsg)
+    {
+        m_robotControlMsg = *(controlMsg.get());
+    }
+
+    geometry_msgs::Twist GetProcessdMsg(){
+        geometry_msgs::Twist currentRobotSpeed;
+        currentRobotSpeed = m_robotOdometryMsg.twist.twist;
+        return m_robotProcessMsg;
+    }
+};
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "Robot_PID_Node");
+    ros::NodeHandle rosHandle;
+
+    pidController pidNode;
+    
+    ros::Publisher cmdVel_pub = rosHandle.advertise<geometry_msgs::Twist>("cmd_vel", 1000);
+    ros::Subscriber odom_sub = rosHandle.subscribe<nav_msgs::Odometry>("deli_robot/odometry/map", 1000, &pidController::OdomCallback, &pidNode);
+    ros::Subscriber control_sub = rosHandle.subscribe<geometry_msgs::Twist>("controlVel", 1000, &pidController::ControlCallback, &pidNode);
+
+    ros::Rate loop_rate(15);
+
+    while(ros::ok()){
+        cmdVel_pub.publish(pidNode.GetProcessdMsg());
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+
+    return 0;
+} 
