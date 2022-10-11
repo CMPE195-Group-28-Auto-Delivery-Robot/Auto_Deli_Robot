d621963157139239c4c346b3d9668ec85d34258f
Ruihua Cai <crh0126@gmail.com>
2022-10-10 21:44:32 -0700

    Add pid node

--- src/position_module/src/pid_processor.cpp
+++ src/position_module/src/pid_processor.cpp
@@ -8,9 +8,11 @@
 
 class pidController{
 private:
-    geometry_msgs::Twist m_robotProcessMsg;
     geometry_msgs::Twist m_robotControlMsg;
     nav_msgs::Odometry m_robotOdometryMsg;
+    const float Kp = 1;
+    const float Ki = 1;
+    const float Kd = 1;
 
 public:
     pidController(){
@@ -28,9 +30,26 @@ public:
     }
 
     geometry_msgs::Twist GetProcessdMsg(){
-        geometry_msgs::Twist currentRobotSpeed;
-        currentRobotSpeed = m_robotOdometryMsg.twist.twist;
+        float headAngle, speedAngle, speed_error;
+        geometry_msgs::Twist m_robotProcessMsg;
+        geometry_msgs::Twist currRobotTwist;
+        geometry_msgs::Pose currRobotPose;
+
+        currRobotTwist = m_robotOdometryMsg.twist.twist;
+        currRobotPose = m_robotOdometryMsg.pose.pose;
+        double currentOrien, currlinSpeed, currAngSpeed;
+        currentOrien = sqrt(pow(currRobotPose.orientation.x,2)+pow(currRobotPose.orientation.y,2));
+        currlinSpeed = sqrt(pow(currRobotTwist.linear.x,2)+pow(currRobotTwist.linear.y,2));
+        m_robotProcessMsg.angular.z = m_robotControlMsg.angular.z;
+        headAngle = atan2(currRobotPose.orientation.y, currRobotPose.orientation.x);
+        speedAngle = atan2(currRobotTwist.linear.y, currRobotTwist.linear.x);
+        if(abs((speedAngle * 180)/M_PI)>90){
+            currlinSpeed *= -1;
+        }
+        speed_error = m_robotControlMsg.linear.x - currAngSpeed;
+        m_robotProcessMsg.linear.x = Kp*speedAngle;
         // Turn the Robot Speed into absoulate speed and retain the Z angler speed only
+        // ROS_INFO("Current Speed: %.2f, %f", currlinSpeed, m_robotControlMsg.angular.z);
         // Compare the current speed with the Input Twist
         return m_robotProcessMsg;
     }
@@ -43,9 +62,9 @@ int main(int argc, char **argv)
 
     pidController pidNode;
     
-    ros::Publisher cmdVel_pub = rosHandle.advertise<geometry_msgs::Twist>("cmd_vel", 1000);
+    ros::Publisher cmdVel_pub = rosHandle.advertise<geometry_msgs::Twist>("controlVel", 1000);
     ros::Subscriber odom_sub = rosHandle.subscribe<nav_msgs::Odometry>("deli_robot/odometry/map", 1000, &pidController::OdomCallback, &pidNode);
-    ros::Subscriber control_sub = rosHandle.subscribe<geometry_msgs::Twist>("controlVel", 1000, &pidController::ControlCallback, &pidNode);
+    ros::Subscriber control_sub = rosHandle.subscribe<geometry_msgs::Twist>("deli_robot/cmd_vel", 1000, &pidController::ControlCallback, &pidNode);
 
     ros::Rate loop_rate(15);
 
