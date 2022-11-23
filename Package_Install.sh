c22b5798b6384f280f05f4378521c7550581ce62
RexCai <crh0126@gmail.com>
2022-11-22 18:21:22 -0800

    Remove Submodule 18x

--- Package_Install.sh
+++ Package_Install.sh
@@ -7,7 +7,11 @@ apt install ros-melodic-tile-map -y
 apt-get install ros-melodic-rplidar-ros -y
 sudo apt install python-tornado python-pip ros-melodic-rosbridge-server ros-melodic-web-video-server nginx -y
 cd src/
-git clone https://github.com/stereolabs/zed-ros-examples.git
+git clone https://github.com/stereolabs/zed-ros-wrapper.git
 git clone https://github.com/stereolabs/zed-ros-interfaces.git
 # git clone https://github.com/osrf/rvizweb/
+git clone https://github.com/CMPE195-Group-28-Auto-Delivery-Robot/garmin18x_usb.git
+cd garmin18x_usb
+git clone https://github.com/quentinsf/pygarmin.git
+cd ..
 cd ..
