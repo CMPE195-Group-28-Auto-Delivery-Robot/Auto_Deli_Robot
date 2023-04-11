b9f8eeba20a3983afc9b29f45c661ba2ae134c5d
Robot <ruihua.cai@sjsu.edu>
2023-04-10 19:47:26 -0700

    Fix Screen Output Bug

--- src/emoji_screen/src/emoji.py
+++ src/emoji_screen/src/emoji.py
@@ -1,48 +1,35 @@
 #!/usr/bin/env python
 
-import os
-import time
+from collections import deque
 import random
 import cv2
-#import pyglet
 from PIL import Image, ImageSequence
 import numpy
 
-import termios
-import atexit
-from select import select
 
 
+N_N = "/home/deli_robot/Auto_Deli_Robot/src/emoji_screen/gif/N_N.gif"
+N_B = "/home/deli_robot/Auto_Deli_Robot/src/emoji_screen/gif/N_B.gif"
 
-N_N = "gif/N_N.gif"
-N_B = "gif/N_B.gif"
+N_L_1 = "/home/deli_robot/Auto_Deli_Robot/src/emoji_screen/gif/N_L_1.gif"
+N_L_2 = "/home/deli_robot/Auto_Deli_Robot/src/emoji_screen/gif/N_L_2.gif"
+N_L_3 = "/home/deli_robot/Auto_Deli_Robot/src/emoji_screen/gif/N_L_3.gif"
+N_R_1 = "/home/deli_robot/Auto_Deli_Robot/src/emoji_screen/gif/N_R_1.gif"
+N_R_2 = "/home/deli_robot/Auto_Deli_Robot/src/emoji_screen/gif/N_R_2.gif"
+N_R_3 = "/home/deli_robot/Auto_Deli_Robot/src/emoji_screen/gif/N_R_3.gif"
 
-N_L_1 = "gif/N_L_1.gif"
-N_L_2 = "gif/N_L_2.gif"
-N_L_3 = "gif/N_L_3.gif"
-N_R_1 = "gif/N_R_1.gif"
-N_R_2 = "gif/N_R_2.gif"
-N_R_3 = "gif/N_R_3.gif"
+E_W_1 = "/home/deli_robot/Auto_Deli_Robot/src/emoji_screen/gif/E_W_1.gif"
+E_W_2 = "/home/deli_robot/Auto_Deli_Robot/src/emoji_screen/gif/E_W_2.gif"
+E_W_3 = "/home/deli_robot/Auto_Deli_Robot/src/emoji_screen/gif/E_W_3.gif"
+E_G_1 = "/home/deli_robot/Auto_Deli_Robot/src/emoji_screen/gif/E_G_1.gif"
+E_G_2 = "/home/deli_robot/Auto_Deli_Robot/src/emoji_screen/gif/E_G_2.gif"
+E_G_3 = "/home/deli_robot/Auto_Deli_Robot/src/emoji_screen/gif/E_G_3.gif"
 
-E_W_1 = "gif/E_W_1.gif"
-E_W_2 = "gif/E_W_2.gif"
-E_W_3 = "gif/E_W_3.gif"
-E_G_1 = "gif/E_G_1.gif"
-E_G_2 = "gif/E_G_2.gif"
-E_G_3 = "gif/E_G_3.gif"
-
-OP = "gif/OP.gif"
-ED = "gif/ED.gif"
-
-screen = cv2.namedWindow("emo", cv2.WINDOW_NORMAL)
-cv2.resizeWindow("emo", 1920, 400)
-cv2.setWindowProperty("emo", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-order_queue = []
-flag = 0
+OP = "/home/deli_robot/Auto_Deli_Robot/src/emoji_screen/gif/OP.gif"
+ED = "/home/deli_robot/Auto_Deli_Robot/src/emoji_screen/gif/ED.gif"
 
 
 def show(path):
-    global screen
     for frame in ImageSequence.Iterator(Image.open(path)):
         frame = frame.convert('RGB')
         cv2_frame = numpy.array(frame)
@@ -71,58 +58,64 @@ def normal():
         show(N_R_3)
 
 
-def error():
-    global order_queue
-    show(E_W_1)
-    while True:
-        show(E_W_2)
-        temp_flag = order_queue.pop()
-        if temp_flag:
-            show(E_W_3)
-            return temp_flag
+class emoji:
 
+    def __init__(self):
+        screen = cv2.namedWindow("emo", cv2.WINDOW_NORMAL)
+        cv2.resizeWindow("emo", 1920, 400)
+        cv2.setWindowProperty("emo", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+        self.order_queue = deque()
+        self.flag = 0
 
-def noGPS():
-    global order_queue
-    show(E_G_1)
-    while True:
-        show(E_G_2)
-        temp_flag = order_queue.pop()
-        if temp_flag:
-            show(E_G_3)
-            return temp_flag
+    def error(self):
+        show(E_W_1)
+        while True:
+            show(E_W_2)
+            if len(self.order_queue) != 0:
+                temp_flag = self.order_queue.popleft()
+                if temp_flag != 3:
+                    show(E_W_3)
+                    return temp_flag
 
+    def noGPS(self):
+        show(E_G_1)
+        while True:
+            show(E_G_2)
+            if len(self.order_queue) != 0:
+                temp_flag = self.order_queue.popleft()
+                if temp_flag != 4:
+                    show(E_G_3)
+                    return temp_flag
 
-def add_order(flag_num):
-    global order_queue
-    order_queue.append(flag_num)
+    def add_order(self, flag_num):
+        self.order_queue.append(flag_num)
 
+    def check_order(self):
+        if len(self.order_queue) == 0:
+            return -1
+        temp = self.order_queue.pop()
+        self.order_queue.append(temp)
+        return temp
 
-def check_order():
-    global flag
-    return flag
-
-
-def run():
-    global order_queue
-    global flag
-    show(OP)
-    while True:
-        if flag != 0:
-            if flag == 3:
-                print("error")
-                flag = error()
-            elif flag == 4:
-                print("noGPS")
-                flag = noGPS()
-            elif flag == 5:
-                print("Program end")
-                show(ED)
-                break
-        else:
-            normal()
-            temp_flag = order_queue.pop()
-            if temp_flag:
-                flag = temp_flag
+    def run(self):
+        show(OP)
+        while True:
+            if self.flag != 0:
+                if self.flag == 3:
+                    print("error")
+                    self.flag = self.error()
+                elif self.flag == 4:
+                    print("noGPS")
+                    self.flag = self.noGPS()
+                elif self.flag == 5:
+                    print("Program end")
+                    show(ED)
+                    cv2.destroyAllWindows()
+                    break
+            else:
+                normal()
+                if len(self.order_queue) != 0:
+                    temp_flag = self.order_queue.popleft()
+                    self.flag = temp_flag
 
 
--- src/emoji_screen/src/emoji_node.py
+++ src/emoji_screen/src/emoji_node.py
@@ -8,37 +8,44 @@ import emoji
 
 
 class emoji_node:
+
     def __init__(self):
-        rospy.init_node('emoji')
-        thread = threading.Thread(target=emoji.run())
-        thread.start()
+        rospy.init_node('emoji_node')
         rospy.Subscriber('/gps', NavSatFix, self.gps_callback)
         rospy.Subscriber('/test', String, self.msg_callback)
+        self.screen = emoji.emoji()
+        self.thread = threading.Thread(target=self.screen.run())
+        self.thread.start()
+        rate = rospy.Rate(1)
+        while not rospy.is_shutdown():
+            rate.sleep()
 
     def gps_callback(self, msg):
         if msg.position_covariance_type == 0:
-            if emoji.check_order != 4:
-                emoji.add_order(4)
-                print("lost gps")
+            if self.screen.check_order() != 4:
+                self.screen.add_order(4)
+                #print("lost gps")
         else:
-            if emoji.check_order == 4:
-                emoji.add_order(0)
-                print("get gps")
+            if self.screen.check_order() != 0:
+                self.screen.add_order(0)
+                #print("get gps")
 
     def msg_callback(self, msg):
         if msg.data == "a":
-            if emoji.check_order != 3:
-                emoji.add_order(3)
-                print("error")
+            if self.screen.check_order() != 3:
+                self.screen.add_order(3)
+                #print("error")
         elif msg.data == "b":
-            if emoji.check_order == 3:
-                emoji.add_order(0)
-                print("pass")
+            if self.screen.check_order() != 0:
+                self.screen.add_order(0)
+                #print("pass")
+        elif msg.data == "c":
+            self.screen.add_order(5)
+            #print("end")
 
 
 if __name__ == '__main__':
     try:
         emoji_node()
-        rospy.spin()
     except rospy.ROSInterruptException:
         pass
